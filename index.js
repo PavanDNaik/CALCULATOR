3fbd866700dc5fdc208af296c43306f4a50adbb3
PavanDNaik <pawannaik396@gmail.com>
2023-08-17 23:08:48 +0530

    done for now

--- index.js
+++ index.js
@@ -13,8 +13,8 @@ function operate(input){
 
     switch(input){
         case 'del':{
-            if(reader.value == "")return;
-            reader.value = reader.value.slice(0,reader.value.length-1);
+            if(reader.textContent == "")return;
+            reader.textContent = reader.textContent.slice(0,reader.textContent.length-1);
             break;
         }
         case 'ac':{
@@ -23,31 +23,31 @@ function operate(input){
             res="";
             opcode="";
             active = true;
-            reader.value = "";
+            reader.textContent = "";
             results.innerHTML = "";
         }
         case '+':
         case '-':
         case '*':
         case '/':{
-            if(reader.value =="")return;            
-            if( active ){
+            if(reader.textContent =="")return;            
+            if( active ){   // active flag represent which operand should be read.
                 opcode = input;
-                op1 = reader.value;
+                op1 = reader.textContent;
                 results.textContent = op1 + input;
-                reader.value = "";
+                reader.textContent = "";
                 active = false;
                 return;
             }
-            else if(op2 =="" && reader.value != ""){
-                op2 = reader.value;
+            else if(op2 =="" && reader.textContent != ""){
+                op2 = reader.textContent;
                 op1 = calculate(op1,op2,opcode);
                 if(op1 == 'e'){
                     operate('ac');
                 }
-                reader.value = op1;
+                reader.textContent = op1;
                 results.textContent +=  op2 + input;
-                clearFlag = true;
+                clearFlag = true;   //clearFlag used to clear reader when we start reading 
                 op2 = "";
                 opcode = input;
             }
@@ -62,7 +62,7 @@ function operate(input){
             {
                 if(op1 == "" )return;
                 if(op2 == ''){
-                    op2 = reader.value;
+                    op2 = reader.textContent;
                 }
                 op1 = calculate(op1,op2,opcode);
                 if(op1 == 'e'){
@@ -70,17 +70,22 @@ function operate(input){
                 }
                 results.textContent += op2;
                 active = true;
-                reader.value = op1;
+                reader.textContent = op1;
                 op1 = "";
                 op2 = "";
+                clearFlag = true;
                 break;
             }
         default:
         if (clearFlag){
-            reader.value ="";
+            reader.textContent ="";
             clearFlag = false;
         }
-        reader.value += input;
+        if(input == '.' && reader.textContent.includes('.'))
+        {
+            return;
+        }
+        reader.textContent += input;
     }
 
 }
